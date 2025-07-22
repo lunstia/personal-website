277a4eecef7d40c3987ee849fcded3b8817ebba7
lunstia <136163666+lunstia@users.noreply.github.com>
2025-07-22 03:45:00 -0400

    New design

--- static/index.js
+++ static/index.js
@@ -5,20 +5,13 @@ async function readResponse(response) {
     
      return new ReadableStream({
       start(controller) {
-        // The following function handles each data chunk
         function push() {
-          // "done" is a Boolean and value a "Uint8Array"
           reader.read().then(({ done, value }) => {
-            // If there is no more data to read
             if (done) {
-              console.log("done", done);
               controller.close();
               return;
             }
-            // Get the data and send it to the browser via the controller
             controller.enqueue(value);
-            // Check chunks by logging to the console
-            console.log(done, value);
             push();
           });
         }
